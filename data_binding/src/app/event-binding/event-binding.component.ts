488fcff333f5d523d4a237d8e152eacb8226cd77
Anukriti-Nawani <anunwn@gmail.com>
2023-05-02 00:14:44 +0530

    data-binding

---
+++ data_binding/src/app/event-binding/event-binding.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-binding',
+  templateUrl: './event-binding.component.html',
+  styleUrls: ['./event-binding.component.css']
+})
+export class EventBindingComponent {
+
+searchValue: string = 'iphone';
+msg:string = 'hi folks';
+name:string = "";
+
+changeSearchValue(eventData:Event) {
+  this.searchValue = (<HTMLInputElement>eventData.target).value;
+}
+
+abc() {
+  console.log("heya, it's anukriti")
+  return this.msg = "heya, it's anukriti";
+}
+
+xyz(nameref:any):void {
+  console.log(nameref)
+this.name=nameref.value;
+}
+
+}
